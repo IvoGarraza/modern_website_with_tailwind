88a038101b48392ca0a4dbd4805441ad4a41e705
ivo garraza <ivogarrachip@gmail.com>
2022-12-24 14:59:46 -0300

    bussines section

--- src/components/Bussines.jsx
+++ src/components/Bussines.jsx
@@ -3,9 +3,19 @@ import {features} from '../constants'
 import styles, {layout} from '../style'
 import Button from './Button'
 
-const FeaturesCard = () =>(
-  <div>
-
+const FeaturesCard = ({icon,  title, content, index}) =>(
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt='icon' className='w-[50%] h-[50%] object-contain'></img>
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
+        {content}
+      </p>
+    </div>
   </div>
 )
 
@@ -20,7 +30,7 @@ const Bussines = () => {
       </div>
       <div className={`${layout.sectionImg} flex-col`}>
         {features.map((features, index)=>(
-          <FeaturesCard key={features.id} {...features}/>
+          <FeaturesCard key={features.id} {...features} index={index}/>
         ))}
       </div>
     </section>
