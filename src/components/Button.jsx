3912c14b834171d54ef5c085722c6745b335f1a4
IvoGarraza <ivogarrachip@gmail.com>
2023-01-02 19:46:43 -0300

    testimonials and clients section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-xl mt-10`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-xl mt-10 hover:text-white`}>
       Get Started
     </button>
   )
